e1ace4444869e243ef243c68563bbe946e0ac718
AJUZZ PC DEBUG <85433436+ajuzzpcdebugs@users.noreply.github.com>
2023-07-10 17:02:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+API_ID = int(environ.get('API_ID', '18455872'))
+API_HASH = environ.get('API_HASH', 'd8e8dbb993d3bc95fa18797b262f7b43')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6282472119:AAHeSHkOxWfF2o4VTBxaLq-III-KsUZifmQ')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
